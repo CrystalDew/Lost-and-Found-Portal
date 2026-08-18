1853c24436f8314cf2b28227c7e19d719cfa76f2
Sharvani Jorwekar <sharvani.jorwekar@gmail.com>
2026-08-19 00:30:05 +0530

    Update db.js

--- db.js
+++ db.js
@@ -1,14 +1,10 @@
-const { Pool } = require('pg');
+const { Pool } = require("pg");
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: {
-    rejectUnauthorized: false // Required for Neon cloud database connection
+    rejectUnauthorized: false // Required for Neon cloud database connections
   }
-});
-
-pool.on('connect', () => {
-  console.log('Connected to PostgreSQL Database (Neon)');
 });
 
 module.exports = pool;
